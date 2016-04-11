46ca309e2bc62797015981db0a0d9382117f666f
David <dhblodgett@gmail.com>
2016-04-11 11:13:14 -0700

    with staticfile code from heroku

--- fredslist/settings.py
+++ fredslist/settings.py
@@ -151,6 +151,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # LOGGING = {
 #     'version': 1,
